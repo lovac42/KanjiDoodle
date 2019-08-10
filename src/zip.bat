c13f2b3c79879e3c8d631a081c4600736659921d
lovac42 <lovac42@protonmail.com>
2019-08-09 21:56:13 -0400

    renamed project

--- src/zip.bat
+++ src/zip.bat
@@ -1,13 +1,13 @@
 @echo off
 set ZIP=C:\PROGRA~1\7-Zip\7z.exe a -tzip -y -r
-set REPO=touch_screen_evo
+set REPO=kanji_doodle
 set VERSION=0.0.1
 
 fsum -r -jm -md5 -d%REPO% * > checksum.md5
 move checksum.md5 %REPO%/checksum.md5
 echo %VERSION% > %REPO%/VERSION
 
-quick_manifest.exe "Touch Screen Evo" "%REPO%" >%REPO%/manifest.json
+quick_manifest.exe "Kanji Doodle" "%REPO%" >%REPO%/manifest.json
 
 REM %ZIP% %REPO%_20.zip *.py %REPO%/*
 
